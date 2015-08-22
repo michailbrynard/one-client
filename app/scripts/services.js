fe3e5dede85826074be97ec71fd8ce5e468a0a67
Dominic Schaff <dominic.schaff@gmail.com>
2015-08-22 17:40:09 +0200

    Adding camera service

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -41,6 +41,24 @@ angular.module('starter.services', ['firebase'])
             */
         };
     })
+    .factory('Camera', ['$q', function($q) {
+        'use strict';
+
+        return {
+        getPicture: function(options) {
+          var q = $q.defer();
+
+          navigator.camera.getPicture(function(result) {
+            // Do any magic you need
+            q.resolve(result);
+          }, function(err) {
+            q.reject(err);
+          }, options);
+
+          return q.promise;
+        }
+        }
+    }])
 
     .service('Auth', function ($window) {
         'use strict';
