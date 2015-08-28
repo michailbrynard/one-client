cff77ae9188acd128f7fe1c72529ad04183b44e6
Helghardt Avenant <helghardt@gmail.com>
2015-08-28 16:55:21 +0200

    Trying alternate file upload.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -102,7 +102,7 @@ angular.module('starter.controllers', [])
                         var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
                         console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
                     }).success(function (data, status, headers, config) {
-                        alert('file ' + config.file.name + 'uploaded. Response: ' + data);
+                        alert('Response: ' + JSON.stringify(data));
                     }).error(function (data, status, headers, config) {
                         alert('error status: ' + status);
                     })
