1f49082036e52780d25a636f5279094745687b95
Helghardt Avenant <helghardt@gmail.com>
2015-08-28 17:33:35 +0200

    Trying alternate file upload.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -75,13 +75,13 @@ angular.module('starter.controllers', [])
             document.addEventListener("deviceready", function () {
 
                 var options = {
-                    quality: 50,
+                    quality: 75,
                     destinationType: Camera.DestinationType.DATA_URL,
                     sourceType: Camera.PictureSourceType.CAMERA,
                     allowEdit: true,
                     encodingType: Camera.EncodingType.JPEG,
-                    targetWidth: 100,
-                    targetHeight: 100,
+                    //targetWidth: 100,
+                    //targetHeight: 100,
                     popoverOptions: CameraPopoverOptions,
                     saveToPhotoAlbum: false
                 };
