0982df6fb543eab6b0bd226066840ec2f09ecd59
Helghardt <helghardt@gmail.com>
2015-08-28 17:09:35 +0200

    Trying alternate file upload.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -115,13 +115,23 @@ angular.module('starter.controllers', [])
         };
 
         $scope.dataURItoBlob = function (dataURI) {
-            var byteString = atob(dataURI.split(',')[1]);
-            var ab = new ArrayBuffer(byteString.length);
-            var ia = new Uint8Array(ab);
+            // convert base64/URLEncoded data component to raw binary data held in a string
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                byteString = atob(dataURI.split(',')[1]);
+            else
+                byteString = decodeURI(dataURI.split(',')[1]);
+
+            // separate out the mime component
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+            // write the bytes of the string to a typed array
+            var ia = new Uint8Array(byteString.length);
             for (var i = 0; i < byteString.length; i++) {
                 ia[i] = byteString.charCodeAt(i);
             }
-            return new Blob([ab], {type: 'image/jpeg'});
+
+            return new Blob([ia], {type: mimeString});
         }
     })
 
