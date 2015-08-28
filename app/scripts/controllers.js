92f22e0850e31bca32b6696f479486bb4bc382c7
Helghardt Avenant <helghardt@gmail.com>
2015-08-28 17:57:19 +0200

    Trying alternate file upload.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -80,8 +80,8 @@ angular.module('starter.controllers', [])
                     sourceType: Camera.PictureSourceType.CAMERA,
                     allowEdit: true,
                     encodingType: Camera.EncodingType.JPEG,
-                    //targetWidth: 100,
-                    //targetHeight: 100,
+                    targetWidth: 800,
+                    targetHeight: 800,
                     popoverOptions: CameraPopoverOptions,
                     saveToPhotoAlbum: false
                 };
