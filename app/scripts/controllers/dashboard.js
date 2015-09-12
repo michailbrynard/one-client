9a00bc656c5d135383e887e613499e7cb4a58142
M Brynard <mbrynard@gmail.com>
2015-09-12 18:40:21 +0200

    Added infinite scroll pagination for dashboard and groups.

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,8 +1,9 @@
 /*global angular, console, window, alert, ionic */
 
 angular.module('starter.controllers.dashboard', [])
-    .controller('MyPhotosCtrl', function ($scope, Images) {
+    .controller('MyPhotosCtrl', function ($scope, $http, Images) {
         'use strict';
+
 
         var refreshData = function () {
             Images.query().success(
@@ -16,13 +17,31 @@ angular.module('starter.controllers.dashboard', [])
                         };
                     }
                     $scope.items = items;
-                    console.log(items)
+                    $scope.nextUrl = rawData.next;
+                    console.log(items);
+                    console.log('URL');
+                    console.log($scope.nextUrl);
                 }
             );
+        };
+
+        $scope.loadNext = function () {
+            $http.get($scope.nextUrl).success(
+                function (rawData) {
+                    for (var i = 0; i < rawData.results.length; i++) {
+                        $scope.items.push({
+                            'imageUrl': rawData.results[i].image.image,
+                            'uploadedAt': rawData.results[i].image.created_timestamp
+                        });
+                        $scope.nextUrl = rawData.next;
+                    }
+                }
+            );
+            $scope.$broadcast('scroll.infiniteScrollComplete');
         };
 
         $scope.$on('$ionicView.afterEnter', function () { // $scope.$on('$destroy'
             console.log('ENTER');
             refreshData();
-        })
+        });
     });
