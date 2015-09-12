708c79a522cefa8a01fa78fbafc46c557a16f08d
M Brynard <mbrynard@gmail.com>
2015-09-12 22:15:17 +0200

    remove contacts for now

--- app/scripts/controllers/groups.js
+++ app/scripts/controllers/groups.js
@@ -129,22 +129,4 @@ angular.module('starter.controllers.groups', [])
                 }
             };
         });
-    })
-
-    .controller('contactsCtrl', function ($scope, $cordovaContacts) {
-        $scope.getContacts = function () {
-            $scope.phoneContacts = [];
-            function onSuccess(contacts) {
-                for (var i = 0; i < contacts.length; i++) {
-                    var contact = contacts[i];
-                    $scope.phoneContacts.push(contact);
-                }
-            };
-            function onError(contactError) {
-                alert(contactError);
-            };
-            var options = {};
-            options.multiple = true;
-            $cordovaContacts.find(options).then(onSuccess, onError);
-        };
     });
